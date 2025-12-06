87c27e1c34f238a083048b31343a2caa5ab90c58
xinnxz <luthfifahmi.alv@gmail.com>
2025-12-06 10:52:53 +0700

    edit tipis tipis

---
+++ dist/profile.DqJha7Z1.js
@@ -0,0 +1,9 @@
+const PROFILE_DATA = {
+  name: "Luthfi Alfaridz Fahmi",
+  tagline: "Membangun masa depan digital dengan kode yang bersih dan indah.",
+  headline: "Inovator Digital dengan Keahlian Penuh Pada Teknologi Web Modern",
+  bio: "Saya Luthfi Alfaridz Fahmi, seorang programmer freelancer dengan fokus mendalam pada pengembangan web full-stack yang modern dan responsif. Dengan pengalaman lebih dari 5 tahun, saya berspesialisasi dalam menciptakan solusi digital yang cepat, aman, dan intuitif. Filosofi saya adalah menggabungkan fungsionalitas optimal dengan estetika desain futuristik, memastikan setiap proyek tidak hanya bekerja dengan sempurna, tetapi juga meninggalkan kesan mendalam. Saya bersemangat untuk mengubah ide kompleks menjadi kenyataan digital yang efisien.",
+  profileImageUrl:
+    "https://spark-builder.s3.us-east-1.amazonaws.com/user-image/2025/12/3/bf9de8fc-37ce-4860-ba0f-18a19bce4715.png",
+};
+export { PROFILE_DATA as P };
